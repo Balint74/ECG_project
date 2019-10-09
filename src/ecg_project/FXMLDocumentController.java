0c225297c3c458c874d0b2d37d2ca5beefa332ee
Fehér Bálint <feherbalint98@freemail.hu>
2019-10-09 11:02:35 +0200

    grafikon vonal vastagsaganak csokkentese

--- src/ecg_project/FXMLDocumentController.java
+++ src/ecg_project/FXMLDocumentController.java
@@ -96,27 +96,15 @@ public class FXMLDocumentController implements Initializable {
         }
         
 
-
         XYChart.Series series = new XYChart.Series();
-        /*
-        series.getData().add(new XYChart.Data(1, 23));
-        series.getData().add(new XYChart.Data(2, 14));
-        series.getData().add(new XYChart.Data(3, 15));
-        series.getData().add(new XYChart.Data(4, 24));
-        series.getData().add(new XYChart.Data(5, 34));
-        series.getData().add(new XYChart.Data(6, 36));
-        series.getData().add(new XYChart.Data(7, 22));
-        series.getData().add(new XYChart.Data(8, 45));
-        series.getData().add(new XYChart.Data(9, 43));
-        series.getData().add(new XYChart.Data(10, 17));
-        series.getData().add(new XYChart.Data(11, 29));
-        series.getData().add(new XYChart.Data(12, 25));
-        */
+
         for(int i = 0; i < 10000;i++){
             
-            series.getData().add(new XYChart.Data(i/1,array.get(i)));
+            series.getData().add(new XYChart.Data(i/10,array.get(i)));
         }
         lineChart.getData().add(series);
+        series.getNode().setStyle("-fx-stroke-width: 1px;");
+
     }
 
     
